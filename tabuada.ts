ceea0276c56f43f30d4ce213c8191ab202815042
DwisssTheLaplace <eduardofernando91475@gmail.com>
2025-08-28 21:22:15 -0300

    Update tabuada.ts

--- tabuada.ts
+++ tabuada.ts
@@ -1,5 +1,5 @@
 // Calculadora de tabuada, não funciona no vscode pois chama o prompt, seria necessario importar uma biblioteca para seu funcionamento apropriado.
-let numero: number = Number(prompt("Insira sobre qual tabuada você deseja saber:"));
+let numero: number = Number(prompt("Insira qual tabuada você deseja saber:"));
 let limite: number = Number(prompt("Até qual multiplicação dessa tabuada você deseja ver?"));
 let i: number = 0;
 
